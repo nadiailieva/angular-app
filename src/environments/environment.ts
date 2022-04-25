c1ab1f9880425dc7b1e587b7878df1e81ea9ad17
nadiailieva <test95422@gmail.com>
2022-04-25 12:12:53 +0300

    update firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBgZwXfF80ZyyfN1nHu5MckoUK3ME1La4M",
-    authDomain: "angular-material-firebas-7ff53.firebaseapp.com",
-    databaseURL: "https://angular-material-firebas-7ff53-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "angular-material-firebas-7ff53",
-    storageBucket: "angular-material-firebas-7ff53.appspot.com",
-    messagingSenderId: "761732858100",
-    appId: "1:761732858100:web:913b1b06f5d91d2a7d9ea1"
+    apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    authDomain: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    databaseURL: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    projectId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    storageBucket: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    messagingSenderId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
   },
 };
